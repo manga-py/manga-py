5a191a6dfa69861289253eb4e4c77f9406764c4f
1271 <sttv-pc@mail.ru>
2017-08-17 14:10:09 +0300

    add unixmanga_nl downloader

--- providers/__init__.py
+++ providers/__init__.py
@@ -37,6 +37,7 @@ providers_list = {
     'readmanga_me': 'readmanga\.me/.+',
     'selfmanga_ru': 'selfmanga\.ru/.+',
     'shakai_ru': 'shakai\.ru/manga.*?/\d+',
+    'unixmanga_nl': 'unixmanga\.nl/onlinereading/.+\.html',
     'yagami_me': 'read\.yagami\.me/.+',
     'yaoichan_me': 'yaoichan\.me/(?:manga|online).+',
     'zingbox_me': 'zingbox\.me/.+',
---
+++ providers/unixmanga_nl.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from lxml.html import document_fromstring
+import re
+
+domainUri = 'http://unixmanga.nl'
+uriRegex = '/onlinereading/([^/.]+)(?:/[^/]\.html|)?'
+
+
+def get_main_content(url, get=None, post=None):
+    name = get_manga_name(url)
+    return get('{}/onlinereading/{}.html'.format(domainUri, name))
+
+
+def get_volumes(content=None, url=None):
+    parser = document_fromstring(content).cssselect('#mycontent table td a[href*="html"]')
+    parser.reverse()
+    return [i.get('href') for i in parser]
+
+
+def get_archive_name(volume, index: int = None):
+    n = volume.rfind('_')
+    if n < 0:
+        return 'c{:0>3}'.format(index)
+    return volume[1 + n: -5]
+
+
+def get_images(main_content=None, volume=None, get=None, post=None):
+    # http://nas.unixmanga.nl/onlinereading/Kaitou Joker/Kaitou Joker c054/001.jpg
+    # http://nas.unixmanga.nl/onlinereading/?image=Kaitou%20Joker/Kaitou%20Joker%20c054/001.jpg&server=nas.html
+    content = get(volume)
+    parser = document_fromstring(content).cssselect('#news a[href*="server="]')
+    images = []
+    for i in parser:
+        img = re.search('(.+)\?image=(.+).server=.+', i.get('href')).groups()
+        images.append(img[0]+img[1])
+    return images
+
+
+def get_manga_name(url, get=None):
+    parser = re.search(uriRegex, url)
+    if not parser:
+        return ''
+    return parser.groups()[0]
+
+
+if __name__ == '__main__':
+    print('Don\'t run this, please!')
