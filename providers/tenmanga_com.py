25cf84406d0849b8da2e89ea8a6e8366249fbad8
1271 <sttv-pc@mail.ru>
2017-08-18 16:13:42 +0300

    patch tenmanga_com

--- providers/tenmanga_com.py
+++ providers/tenmanga_com.py
@@ -25,18 +25,24 @@ def get_images(main_content=None, volume=None, get=None, post=None):
     items = document_fromstring(get(volume)).cssselect('.pic_box img.manga_pic')
     return [i.get('src') for i in items]
 
+def __get_name(test):
+    test = test.groups()[0]
+    if test.rfind('.html') > 0:
+        test = test[0:test.rfind('.html')]
+    return test.replace('+', ' ')
+
 
 def get_manga_name(url, get=None):
     _ = '\.com/book/([^/]+)'
     test = re.search(_, url)
     if test:
-        return test.groups()[0]
+        return __get_name(test)
     test = re.search('\.com/chapter/.+', url)
     if test:
         test = document_fromstring(get(url)).cssselect('.read-page nav a[href*="/book/"]')
         if len(test):
             test = re.search(_, test[0].get('href'))
-            return test.groups()[0]
+            return __get_name(test)
     return ''
 
 
