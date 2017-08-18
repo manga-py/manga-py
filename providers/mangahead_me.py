82b805f863df4aee1c2aad0918c54e8fa40949af
1271 <sttv-pc@mail.ru>
2017-08-18 10:25:26 +0300

    pages patch

--- providers/mangahead_me.py
+++ providers/mangahead_me.py
@@ -25,13 +25,22 @@ def get_archive_name(volume, index: int = None):
 
 
 def get_images(main_content=None, volume=None, get=None, post=None):
+    tmb_selector = '#main_content .mangahead_thumbnail_cell a[name]'
     content = get(volume)
-    parser = document_fromstring(content).cssselect('#main_content .mangahead_thumbnail_cell a[name]')
+    parser = document_fromstring(content).cssselect(tmb_selector)
     if len(parser) < 1:
         return []
+    pages = document_fromstring(content).cssselect('table + .mangahead_pages_navigator a:not(:last-child)')
     _ = document_fromstring(get(domainUri + parser[0].get('href'))).cssselect('#mangahead_image')[0].get('src')
     img_uri = _[0: 1 + _.rfind('/')]
-    return [img_uri + i.get('name') for i in parser]
+    images = [img_uri + i.get('name') for i in parser]
+    if len(pages):
+        for i in pages:
+            content = get(domainUri + i.get('href'))
+            parser = document_fromstring(content).cssselect(tmb_selector)
+            if len(parser):
+                images += [img_uri + i.get('name') for i in parser]
+    return images
 
 
 def get_manga_name(url, get=None):
