7c3915b9910c5c304555de42e7bdeb08bbc8d7a1
1271 <sttv-pc@mail.ru>
2017-08-28 01:34:51 +0300

    patch crop script
    Signed-off-by: 1271 <sttv-pc@mail.ru>

--- helpers/remove_void/__init__.py
+++ helpers/remove_void/__init__.py
@@ -1,102 +1,80 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# import os
-from PIL import Image, ImageDraw
+from PIL import Image
 
 _image = None
 
 
 # I'm writing this crap because i can not think of anything better at this moment
 def _test_pixel(max_s_s: int, factor: int, mode: int):
-    width = _image.size[0] - 1
-    height = _image.size[1] - 1
+    width = _image.size[0]
+    height = _image.size[1]
     if height < max_s_s or width < max_s_s:
         return 0
     pix = _image.load()
     if mode == 0:  # left
-        print([range(max_s_s), range(height)])
         for j in range(max_s_s):
             for i in range(height):
                 _ = pix[j, i]
-                if _ == 0:
-                    continue
                 a = _[0]
                 b = _[1]
                 c = _[2]
                 S = a + b + c
-                print([S, (((255 + factor) // 2) * 3)])
-                if S > (((255 + factor) // 2) * 3):
+                if S < (((255 + factor) // 2) * 3):
                     return j
     elif mode == 1:  # top
         for i in range(max_s_s):
             for j in range(width):
                 _ = pix[j, i]
-                if _ == 0:
-                    continue
                 a = _[0]
                 b = _[1]
                 c = _[2]
                 S = a + b + c
-                print([S, (((255 + factor) // 2) * 3)])
-                if S > (((255 + factor) // 2) * 3):
+                if S < (((255 + factor) // 2) * 3):
                     return i
     if mode == 2:  # right
         for j in range(max_s_s):
             for i in range(height):
-                _ = pix[width - j, i]
-                if _ == 0:
-                    continue
+                w = width - j - 1
+                _ = pix[w, i]
                 a = _[0]
                 b = _[1]
                 c = _[2]
                 S = a + b + c
-                print([S, (((255 + factor) // 2) * 3)])
-                if S > (((255 + factor) // 2) * 3):
-                    return j
+                if S < (((255 + factor) // 2) * 3):
+                    return w
     elif mode == 3:  # bottom
         for i in range(max_s_s):
             for j in range(width):
-                _ = pix[j, height - i]
-                if _ == 0:
-                    continue
+                h = height - i - 1
+                _ = pix[j, h]
                 a = _[0]
                 b = _[1]
                 c = _[2]
                 S = a + b + c
-                print([S, (((255 + factor) // 2) * 3)])
-                if S > (((255 + factor) // 2) * 3):
-                    return i
+                if S < (((255 + factor) // 2) * 3):
+                    return h
     return 0
 
 
 def _get_crop_sizes(factor: int, max_s_s: int):
-    """
-    :return: ImageDraw.Draw
-    """
     left = _test_pixel(max_s_s, factor, 0)
-    print(left)
     top = _test_pixel(max_s_s, factor, 1)
-    print(top)
     right = _test_pixel(max_s_s, factor, 2)
-    print(right)
     bottom = _test_pixel(max_s_s, factor, 3)
-    print(bottom)
     return left, top, right, bottom
 
 
 def _open_image(path: str):
-    """
-    :param path: absolute img path
-    :return: bool
-    """
     global _image
     try:
         _image = Image.open(path)
+        if _image.mode != 'RGB':  # fixed image mode
+            _image = _image.convert('RGB')
     except OSError:
         return False
     return True
-
 
 
 def process(img_path, img_out_path, factor: int = 100, maximum_side_size: int = 30):
@@ -104,19 +82,11 @@ def process(img_path, img_out_path, factor: int = 100, maximum_side_size: int = 
         return False
     _ = _image.load()
     sizes = _get_crop_sizes(factor, maximum_side_size)
-    # Image.crop( (left, upper, right, lower, ) )
-    # print(sizes)
-    exit()
-    if sizes[2] == 0 or sizes[1] == 0:
+    if sizes[2] == 0 or sizes[3] == 0:
         return False
     image = _image.crop(sizes)
-    # print(image)
-    # exit()
     image.save(img_out_path)
     return True
-
-
-print(process('./7ce65ff9daf3512829763b91cb41ef37.jpg', './img2.png'))
 
 
 if __name__ == '__main__':
