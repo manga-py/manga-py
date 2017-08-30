6ad71bef0dfb98b9b7ce12d3bf2a5e8fec9e535f
1271 <sttv-pc@mail.ru>
2017-08-30 23:29:44 +0300

    +develop comic_walker_com

---
+++ providers/comic_walker_com.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# from lxml.html import document_fromstring
+# import re
+# import json
+
+domainUri = 'http://comic-walker.com'
+
+
+def get_main_content(url, get=None, post=None):
+    """
+    :param url: str
+    :param get: request.get
+    :param post: request.post
+    :return: mixed (1)
+    """
+    pass
+
+
+def get_volumes(content=None, url=None):
+    """
+    :param content: mixed (1)
+    :param url: str
+    :return: array (2)
+    """
+    pass
+
+
+def get_archive_name(volume, index: int = None):
+    """
+    :param volume: mixed (2)
+    :param index: int
+    :return: str
+    """
+    pass
+
+
+def get_images(main_content=None, volume=None, get=None, post=None):
+    """
+    :param main_content: mixed (1)
+    :param volume: mixed (2)
+    :param get: request.get
+    :param post: request.post
+    :return: dict(str)
+    """
+    pass
+
+
+def get_manga_name(url, get=None):
+    """
+    :param url: str
+    :param get: request.get
+    :return: str
+    """
+    pass
+
+
+if __name__ == '__main__':
+    print('Don\'t run this, please!')
