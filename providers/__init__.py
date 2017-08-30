0a51bd4b6d637f65a0cb261eaebea3dcd85d2e7f
1271 <sttv-pc@mail.ru>
2017-08-30 14:23:16 +0300

    add mangalib_me downloader

--- providers/__init__.py
+++ providers/__init__.py
@@ -29,6 +29,7 @@ providers_list = {
     'mangainn_net': 'mangainn\.net/manga/.+',
     'mangakakalot_com': 'mangakakalot\.com/(manga|chapter)/.+',
     'mangaleader_com': 'mangaleader\.com/read\-.+',
+    'mangalib_me': 'mangalib\.me/.+',
     'mangamove_com': 'mangamove\.com/manga/.+',
     'manganel_com': 'manganel\.com/(manga|chapter)/.+',
     'mangaonlinehere_com': 'mangaonlinehere\.com/manga\-info/.+',
--- providers/mangalib_me.py
+++ providers/mangalib_me.py
@@ -1,80 +1,53 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# from lxml.html import document_fromstring
-# import re
-# import json
+from lxml.html import document_fromstring
+import re
+import json
+
+domainUri = 'https://mangalib.me'
 
 
 def get_main_content(url, get=None, post=None):
-    """
-    :param url: str
-    :param get: request.get
-    :param post: request.post
-    :return: mixed (1)
-    """
-    pass
+    return get('{}/{}'.format(domainUri, get_manga_name(url)))
 
 
 def get_volumes(content=None, url=None):
-    """
-    :param content: mixed (1)
-    :param url: str
-    :return: array (2)
-    """
-    pass
+    items = document_fromstring(content).cssselect('.vol_lst li h5 a')
+    return [i.get('href') for i in items]
 
 
 def get_archive_name(volume, index: int = None):
-    """
-    :param volume: mixed (2)
-    :param index: int
-    :return: str
-    """
-    pass
+    name = re.search('\.me/[^/]+/([^/]+/[^/]+)', volume)
+    if not name:
+        return 'vol_{}'.format(index)
+    return name.groups()[0]
 
 
 def get_images(main_content=None, volume=None, get=None, post=None):
-    """
-    :param main_content: mixed (1)
-    :param volume: mixed (2)
-    :param get: request.get
-    :param post: request.post
-    :return: dict(str)
-    """
-    pass
+    content = get(volume)
+    _id = re.search('var\s+currentId.+?=.+?(\d+)', content)
+    if not _id:
+        return []
+    try:
+        uri = domainUri + '/pages/' + _id.groups()[0]
+        items = json.loads(get(uri))
+    except Exception:
+        return []
+    href = re.search('[\'"](http[^\'"]+)[\'"].+\.page_image', content)
+    if not href:
+        _ = re.search('(\d+)/.?(\d+)', volume).groups()
+        href = 'https://img1.mangalib.me/manga/one-piece/chapters/' + _[0] + '-' + _[1]
+    else:
+        href = href.groups()[0].strip('/') + '/'
+    return [href + i['page_image'] for i in items['pages']]
 
 
 def get_manga_name(url, get=None):
-    """
-    :param url: str
-    :param get: request.get
-    :return: str
-    """
-    pass
-
-
-# NOT REQUIRED /*
-
-# if True, use zip_list(). get_images() alternative
-download_zip_only = None
-
-
-def get_zip(main_content=None, volume=None, get=None, post=None):
-    """
-    :param main_content: mixed (1)
-    :param volume: mixed (2)
-    :param get: request.get
-    :param post: request.post
-    :return: str|str[]
-    """
-    pass
-
-# if not None - additional cookies
-# cookies = [{'value': 'cookie value','domain': 'asd.domain','path': '/cookie/path/','name': 'cookie_name',}, 'Browser']
-cookies = None
-
-# */ NOT REQUIRED
+    name = re.search('\.me/([^/]+)', url)
+    if not name:
+        return ''
+    return name.groups()[0]
 
 
 if __name__ == '__main__':
