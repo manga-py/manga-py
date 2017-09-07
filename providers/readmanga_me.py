1536507c4de397bc358861fa01474ae2c0b8ba08
1271 <sttv-pc@mail.ru>
2017-09-07 11:59:23 +0300

    patch readmanga_me, mintmanga_com

--- providers/readmanga_me.py
+++ providers/readmanga_me.py
@@ -11,7 +11,7 @@ domainUri = 'http://readmanga.me'
 def get_main_content(url, get=None, post=None):
     name = get_manga_name(url)
     url = '{}/{}'.format(domainUri, name)
-    return get(url + '?mature=1')
+    return get(url + '?mature=1&mtr=1')
 
 
 def get_volumes(content: str, url=None, get=None, post=None):
