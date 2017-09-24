028f8b57b51ca29fba948ad54f5676d258bbf24a
1271 <sttv-pc@mail.ru>
2017-09-25 00:24:27 +0300

    Don't use this version!

--- manga.py
+++ manga.py
@@ -14,15 +14,13 @@ from argparse import ArgumentParser
 from urllib.parse import urlparse
 from helpers import remove_void
 from helpers.exceptions import *
+from threading import Thread
 
 __author__ = 'Sergey Zharkov'
 __license__ = 'MIT'
 __email__ = 'sttv-pc@mail.ru'
-__version__ = '0.2.1.0'
-
-
-_downloader_uri = 'https://github.com/yuru-yuri/Manga-Downloader'
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+__version__ = '0.2.1.1b'
+__downloader_uri__ = 'https://github.com/yuru-yuri/Manga-Downloader'
 
 if os.name == 'nt':
     tty_rows = 0
@@ -30,16 +28,12 @@ if os.name == 'nt':
 else:
     tty_rows, tty_columns = os.popen('stty size', 'r').read().split()
 
+count_retries = 5
 rnd_temp_path = 'manga-donloader_{}'.format(random.random()*10)
 archivesDir = os.path.join(os.getcwd(), 'manga')
 
-count_retries = 5
 
-referrer_url = ''
-site_cookies = ()
-
-
-def _arguments_parser():  # pragma: no cover
+def _arguments_parser() -> ArgumentParser:  # pragma: no cover
     """
     Arguments parser helper
     """
@@ -72,13 +66,12 @@ def _arguments_parser():  # pragma: no cover
     return parse
 
 
-
 @atexit.register
 def before_shutdown():
     shutil.rmtree(get_temp_path())
 
 
-def get_temp_path(path: str = ''):
+def get_temp_path(path: str = '') -> str:
     rnd_dir = os.path.join(tempfile.gettempdir(), rnd_temp_path)
     if not os.path.isdir(rnd_dir):
         os.makedirs(rnd_dir)
@@ -92,24 +85,99 @@ class VariablesHelper:
     main_content = ''
     status = False
     provider = None
+    site_cookies = {}
+    referrer_url = ''
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36'
+
+
+class MultiThreadsStorage:
+
+    _urls = []
+    _root_path = None
+
+    def add_url(self, url: str, full_path_name: str):
+        full_path = os.path.dirname(full_path_name)
+        basename = MangaDownloader.prepare_file_name(os.path.basename(full_path_name))
+        self._urls.append({'url': url, 'name': os.path.join(full_path, basename)})
+
+    def set_root_path(self, root_path: str) -> bool:
+        self._root_path = root_path
+        return True
+
+    def _full_path_builder(self, params: dict) -> str:
+        params.setdefault('root', False)
+        _name = RequestsHelper.remove_file_name_params(params['name'])
+        if params['root']:
+            _root_path = self._root_path.rstrip('/')
+        else:
+            _root_path = os.getcwd()
+        return os.path.join(_root_path, _name.lstrip('/'))
+
+    def add_urls(self, params):
+        """
+        :param params: [
+         {url: http://example.org, name: path/to/file.png},  # path = os.getcwd()/path/to/file.png
+         {url: http://example.org, name: /path/to/file.png},  # path = os.getcwd()/path/to/file.png
+         {url: http://example.org, name: /path/to/file.png, root: True}  # path = self.path/path/to/file.png
+         {url: http://example.org, name: file.png, root: True}  # path = self.path/file.png
+        ]
+        :return:
+        """
+        if not isinstance(params, list):
+            return False
+        for i in params:
+            if hasattr(i, 'url'):
+                path = self._full_path_builder(i)
+                self.add_url(i['url'], path)
+
+    def get_urls(self) -> list:
+        """
+        :return: [
+         {url: http://example.org, name: /full/path/to/file.png},
+         url: http://example.org, name: /full/path/to/file2.png}
+        ]
+        """
+        return self._urls
+
+
+class MultiThreads:
+
+    threads = []
+
+    def addThread(self, target: function, args: tuple):
+        self.threads.append(Thread(target, args))
+
+    def startAll(self):
+        for t in self.threads:  # starting all threads
+            t.start()
+        for t in self.threads:  # joining all threads
+            t.join()
 
 
 class RequestsHelper(VariablesHelper):
 
     @staticmethod
-    def __safe_downloader_url_helper(url):
+    def remove_file_name_params(name, save_path = True) -> str:
+        file_path = os.path.dirname(name)
+        name = os.path.basename(name)
+        if name.find('?') > 0:
+            name = name[0:name.find('?')]
+        return os.path.join(file_path, name) if save_path else name
+
+    def __safe_downloader_url_helper(self, url) -> str:
         if url.find('//') == 0:
-            return 'http:' + url
+            _ = urlparse(self.referrer_url)
+            return _.scheme + ':' + url
         if url.find('://') < 1:
-            _ = referrer_url
+            _ = self.referrer_url[0:self.referrer_url.rfind('/')]
             if url.find('/') == 0:
-                _ = urlparse(referrer_url)
+                _ = urlparse(self.referrer_url)
                 _ = '{}://{}'.format(_.scheme, _.netloc)
-            return _ + url
+            return _.rstrip('/') + '/' + url.lstrip('/')
         return url
 
     # fast fixed #5
-    def __requests_helper(self, method, url, headers=None, cookies=None, data=None, files=None, max_redirects=10):
+    def __requests_helper(self, method, url, headers=None, cookies=None, data=None, files=None, max_redirects=10) -> requests.Response:
         r = getattr(requests, method)(url=url, headers=headers, cookies=cookies, data=data, files=files, allow_redirects=False)
         if r.is_redirect:
             if max_redirects < 1:
@@ -117,18 +185,18 @@ class RequestsHelper(VariablesHelper):
             return self.__requests_helper(method, r.headers['location'], headers, cookies, data, files, max_redirects-1)
         return r
 
-    def __requests(self, url: str, headers: dict=None, cookies: dict=None, data=None, method='get', files=None):
+    def __requests(self, url: str, headers: dict=None, cookies: dict=None, data=None, method='get', files=None) -> requests.Response:
         if not headers:
             headers = {}
         if not cookies:
-            cookies = site_cookies
-        headers.setdefault('User-Agent', user_agent)
-        headers.setdefault('Referer', referrer_url)
+            cookies = self.site_cookies
+        headers.setdefault('User-Agent', self.user_agent)
+        headers.setdefault('Referer', self.referrer_url)
         if arguments.allow_webp:
             headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
         return self.__requests_helper(method=method, url=url, headers=headers, cookies=cookies, data=data, files=files)
 
-    def _get(self, url: str, headers: dict=None, cookies: dict=None, offset: int = -1, maxlen: int = -1):
+    def _get(self, url: str, headers: dict=None, cookies: dict=None, offset: int = -1, maxlen: int = -1) -> str:
         response = self.__requests(url=url, headers=headers, cookies=cookies, method='get')
         ret = response.text
         response.close()
@@ -138,13 +206,13 @@ class RequestsHelper(VariablesHelper):
             ret = ret[:maxlen]
         return ret
 
-    def _post(self, url: str, headers: dict=None, cookies: dict=None, data: dict = (), files=None):
+    def _post(self, url: str, headers: dict=None, cookies: dict=None, data: dict = (), files=None) -> str:
         response = self.__requests(url=url, headers=headers, cookies=cookies, method='post', data=data, files=files)
         text = response.text
         response.close()
         return text
 
-    def _safe_downloader(self, url, file_name):
+    def _safe_downloader(self, url, file_name) -> bool:
         try:
             url = self.__safe_downloader_url_helper(url)
             response = self.__requests(url, method='get')
@@ -156,21 +224,22 @@ class RequestsHelper(VariablesHelper):
         except OSError:
             return False
 
-    def _get_cookies(self, url: str):
+    def _multi_threads_downloader(self, params: list, callback: function):
+        pass
+
+    def _prepare_cookies(self, url: str):
         session = requests.Session()
         h = session.head(url)
-        global site_cookies
         cookies_exists = hasattr(self.provider, 'cookies') and getattr(self.provider, 'cookies')
         if self.status and cookies_exists:
             cookies = getattr(self.provider, 'cookies')
             for i in cookies:
                 if isinstance(i, str):
-                    global user_agent
-                    user_agent = i
+                    self.user_agent = i
                 else:
                     h.cookies.set(i['name'], i['value'], domain=i['domain'], path=i['path'])
         session.close()
-        site_cookies = h.cookies
+        self.site_cookies = h.cookies
 
 
 class ImageHelper:
@@ -229,10 +298,8 @@ class MangaDownloader(RequestsHelper, ImageHelper):
         return name
 
     @staticmethod
-    def _download_image_name_helper(temp_path, i, n):
-        name = os.path.basename(i)
-        if name.find('?') > 0:
-            name = name[0:name.find('?')]
+    def _download_image_name_helper(temp_path, i, n) -> str:
+        name = RequestsHelper.remove_file_name_params(i)
         basename = '{:0>3}_{}'.format(n, name)
         name_question = name.find('?') == 0
         name_len = len(name) < 4
@@ -249,12 +316,14 @@ class MangaDownloader(RequestsHelper, ImageHelper):
             self.name = self.provider.get_manga_name(self.url, get=self._get)
         self._make_manga_dir()
 
-        global referrer_url
-        ref = urlparse(url)
-        referrer_url = '{}://{}'.format(ref.scheme, ref.netloc)
-        self._get_cookies(referrer_url)
+        if len(arguments.user_agent):
+            self.user_agent = arguments.user_agent
 
-    def _download_image(self, url, path):
+        ref = urlparse(url)
+        self.referrer_url = '{}://{}'.format(ref.scheme, ref.netloc)
+        self._prepare_cookies(self.referrer_url)
+
+    def _download_image(self, url, path) -> bool:
         r = 0
         while r < count_retries:
             if self._safe_downloader(url, path):
@@ -336,7 +405,7 @@ class MangaDownloader(RequestsHelper, ImageHelper):
             file = os.path.join(temp_directory, f)
             if os.path.isfile(file):
                 archive.write(file, f)
-        archive.writestr('info.txt', 'Site: {}\nDownloader: {}'.format(self.url, _downloader_uri))
+        archive.writestr('info.txt', 'Site: {}\nDownloader: {}'.format(self.url, __downloader_uri__))
         archive.close()
 
     def _download_images_helper(self, v, volume_index):
@@ -456,8 +525,6 @@ if __name__ == '__main__':  # pragma: no cover
         arguments = _arguments_parser().parse_args()
         add_name = not arguments.no_name
         name = arguments.name
-        if len(arguments.user_agent):
-            user_agent = arguments.user_agent
 
         if arguments.url:
             url = arguments.url
