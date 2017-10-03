3753b7267e24f07ef50cf33b4dc2db9a6ad48eb4
1271 <sttv-pc@mail.ru>
2017-10-03 16:23:34 +0300

    more code mangaz_com

--- providers/mangaz_com.py
+++ providers/mangaz_com.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# from lxml.html import document_fromstring
-# import re
+from lxml.html import document_fromstring
+import re
 # import json
 
 
@@ -52,26 +52,3 @@ def get_manga_name(url, get=None):
     :return: str
     """
     pass
-
-
-# NOT REQUIRED /*
-
-# if True, use zip_list(). get_images() alternative
-download_zip_only = None
-
-
-def get_zip(main_content=None, volume=None, get=None, post=None):
-    """
-    :param main_content: mixed (1)
-    :param volume: mixed (2)
-    :param get: request.get
-    :param post: request.post
-    :return: str|str[]
-    """
-    pass
-
-# if not None - additional cookies
-# cookies = [{'value': 'cookie value','domain': 'asd.domain','path': '/cookie/path/','name': 'cookie_name',}, 'Browser']
-cookies = None
-
-# */ NOT REQUIRED
