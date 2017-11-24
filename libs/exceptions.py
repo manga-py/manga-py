40cc2f770683385e7f2e8b11934115db7f2d059e
1271 <sttv-pc@mail.ru>
2017-11-24 21:25:45 +0300

    Init

---
+++ libs/exceptions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+class DirectoryNotWritable(AttributeError):
+    pass
+
+
+class DirectoryNotExists(AttributeError):
+    pass
+
+
+class VolumesNotFound(AttributeError):
+    pass
+
+
+class StatusError(AttributeError):
+    pass
+
+
+class UrlParseError(AttributeError):
+    pass
+
+
+class ProviderNotFound(AttributeError):
+    pass
+
+
+class Print(Exception):
+    pass
---
+++ libs/parser.py
@@ -0,0 +1,4 @@
+class Parser:
+
+    def __init__(self):
+        pass
