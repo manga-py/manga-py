689237a401553c1f3efa9073ebf2d6829f97be10
1271 <sttv-pc@mail.ru>
2017-12-21 23:51:42 +0300

    manhuagui_com_js patch (manga.exe exception)

--- helpers/main/crypt.py
+++ helpers/main/crypt.py
@@ -52,10 +52,8 @@ def kissmanga(iv, key, data):
 
 
 def manhuagui(js, default=''):
-    script = path.join(path.dirname(path.realpath(__file__)), 'manhuagui_com.js')
     try:
-        with open(script, 'r') as LZjs:
-            return execjs.compile(LZjs.read()).eval(js)
+        return execjs.compile(get_manhuagui_com_js()).eval(js)
     except execjs._exceptions.RuntimeUnavailableError:
         print('Could not find an available JavaScript runtime. Need install Node.js')
         return default
@@ -145,3 +143,174 @@ class ComicWalker:
             j = (j + s[i] + key[i % len(key)]) & 0xff
             s[i], s[j] = s[j], s[i]
         return s
+
+
+def get_manhuagui_com_js():
+    return """
+    var LZString = (function () {
+    var f = String.fromCharCode;
+    var keyStrBase64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+    var baseReverseDic = {};
+
+    function getBaseValue(alphabet, character) {
+        if (!baseReverseDic[alphabet]) {
+            baseReverseDic[alphabet] = {};
+            for (var i = 0; i < alphabet.length; i++) {
+                baseReverseDic[alphabet][alphabet.charAt(i)] = i
+            }
+        }
+        return baseReverseDic[alphabet][character]
+    }
+
+    var LZString = {
+        decompressFromBase64: function (input) {
+            if (input == null) return "";
+            if (input == "") return null;
+            return LZString._0(input.length, 32, function (index) {
+                return getBaseValue(keyStrBase64, input.charAt(index))
+            })
+        }, _0: function (length, resetValue, getNextValue) {
+            var dictionary = [], next, enlargeIn = 4, dictSize = 4, numBits = 3, entry = "", result = [], i, w, bits,
+                resb, maxpower, power, c, data = {val: getNextValue(0), position: resetValue, index: 1};
+            for (i = 0; i < 3; i += 1) {
+                dictionary[i] = i
+            }
+            bits = 0;
+            maxpower = Math.pow(2, 2);
+            power = 1;
+            while (power != maxpower) {
+                resb = data.val & data.position;
+                data.position >>= 1;
+                if (data.position == 0) {
+                    data.position = resetValue;
+                    data.val = getNextValue(data.index++)
+                }
+                bits |= (resb > 0 ? 1 : 0) * power;
+                power <<= 1
+            }
+            switch (next = bits) {
+                case 0:
+                    bits = 0;
+                    maxpower = Math.pow(2, 8);
+                    power = 1;
+                    while (power != maxpower) {
+                        resb = data.val & data.position;
+                        data.position >>= 1;
+                        if (data.position == 0) {
+                            data.position = resetValue;
+                            data.val = getNextValue(data.index++)
+                        }
+                        bits |= (resb > 0 ? 1 : 0) * power;
+                        power <<= 1
+                    }
+                    c = f(bits);
+                    break;
+                case 1:
+                    bits = 0;
+                    maxpower = Math.pow(2, 16);
+                    power = 1;
+                    while (power != maxpower) {
+                        resb = data.val & data.position;
+                        data.position >>= 1;
+                        if (data.position == 0) {
+                            data.position = resetValue;
+                            data.val = getNextValue(data.index++)
+                        }
+                        bits |= (resb > 0 ? 1 : 0) * power;
+                        power <<= 1
+                    }
+                    c = f(bits);
+                    break;
+                case 2:
+                    return ""
+            }
+            dictionary[3] = c;
+            w = c;
+            result.push(c);
+            while (true) {
+                if (data.index > length) {
+                    return ""
+                }
+                bits = 0;
+                maxpower = Math.pow(2, numBits);
+                power = 1;
+                while (power != maxpower) {
+                    resb = data.val & data.position;
+                    data.position >>= 1;
+                    if (data.position == 0) {
+                        data.position = resetValue;
+                        data.val = getNextValue(data.index++)
+                    }
+                    bits |= (resb > 0 ? 1 : 0) * power;
+                    power <<= 1
+                }
+                switch (c = bits) {
+                    case 0:
+                        bits = 0;
+                        maxpower = Math.pow(2, 8);
+                        power = 1;
+                        while (power != maxpower) {
+                            resb = data.val & data.position;
+                            data.position >>= 1;
+                            if (data.position == 0) {
+                                data.position = resetValue;
+                                data.val = getNextValue(data.index++)
+                            }
+                            bits |= (resb > 0 ? 1 : 0) * power;
+                            power <<= 1
+                        }
+                        dictionary[dictSize++] = f(bits);
+                        c = dictSize - 1;
+                        enlargeIn--;
+                        break;
+                    case 1:
+                        bits = 0;
+                        maxpower = Math.pow(2, 16);
+                        power = 1;
+                        while (power != maxpower) {
+                            resb = data.val & data.position;
+                            data.position >>= 1;
+                            if (data.position == 0) {
+                                data.position = resetValue;
+                                data.val = getNextValue(data.index++)
+                            }
+                            bits |= (resb > 0 ? 1 : 0) * power;
+                            power <<= 1
+                        }
+                        dictionary[dictSize++] = f(bits);
+                        c = dictSize - 1;
+                        enlargeIn--;
+                        break;
+                    case 2:
+                        return result.join('')
+                }
+                if (enlargeIn == 0) {
+                    enlargeIn = Math.pow(2, numBits);
+                    numBits++
+                }
+                if (dictionary[c]) {
+                    entry = dictionary[c]
+                } else {
+                    if (c === dictSize) {
+                        entry = w + w.charAt(0)
+                    } else {
+                        return null
+                    }
+                }
+                result.push(entry);
+                dictionary[dictSize++] = w + entry.charAt(0);
+                enlargeIn--;
+                w = entry;
+                if (enlargeIn == 0) {
+                    enlargeIn = Math.pow(2, numBits);
+                    numBits++
+                }
+            }
+        }
+    };
+    return LZString
+})();
+String.prototype.splic = function (f) {
+    return LZString.decompressFromBase64(this).split(f)
+};
+    """
