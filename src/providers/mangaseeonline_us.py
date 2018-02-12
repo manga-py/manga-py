98bd2793eda22e2c4325cc8a470358c44dec8936
1271 <sttv-pc@mail.ru>
2018-02-12 13:25:58 +0300

    add mangaseeonline_us provider

---
+++ src/providers/mangaseeonline_us.py
@@ -0,0 +1,29 @@
+from src.provider import Provider
+
+
+class _Template(Provider):
+
+    def get_archive_name(self) -> str:
+        pass
+
+    def get_chapter_index(self) -> str:
+        pass
+
+    def get_main_content(self):
+        pass
+
+    def get_manga_name(self) -> str:
+        return ''
+
+    def get_chapters(self):
+        return []
+
+    def get_files(self):
+        return []
+
+    def get_cover(self) -> str:
+        pass
+        # return self._get_cover_from_content('.cover img')
+
+
+main = _Template
