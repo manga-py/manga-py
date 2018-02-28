7e4f2689ebcd368dc3a31a49570f8dd79dca1ba5
1271 <sttv-pc@mail.ru>
2018-02-28 10:21:11 +0300

    Oops

--- src/providers/pururin_io.py
+++ src/providers/pururin_io.py
@@ -5,27 +5,53 @@ from .helpers.std import Std
 class _Template(Provider, Std):
 
     def get_archive_name(self) -> str:
-        pass
+        return 'archive'
 
     def get_chapter_index(self) -> str:
-        pass
+        return '0'
 
     def get_main_content(self):
-        pass
+        url = self.get_url()
+        if url.find('/gallery/') > 0:
+            re = r'/gallery/(\d+)/([^/]+)'
+        else:
+            re = r'/read/(\d+)/\d+/([^/]+)'
+        return self._get_content('{}/gallery/{}/{}'.format(
+            self.get_domain(),
+            *self.re.search(re, url).groups()
+        ))
 
     def get_manga_name(self) -> str:
-        return ''
+        url = self.get_url()
+        if url.find('/gallery/') > 0:
+            re = r'/gallery/\d+/([^/]+)'
+        else:
+            re = r'/read/\d+/\d+/([^/]+)'
+        return self._get_name(re)
 
     def get_chapters(self):
-        # return self._elements('a.chapter')
-        return []
+        return [b'']
+
+    def _images(self, content):
+        items = self.json.loads(content)
+        images = []
+        for i in items:
+            url = items.get(i, {}).get('image', False)
+            url and images.append(url)
+        return images
 
     def get_files(self):
+        items = self._elements('.col-md-10 .well-pururin > div[class*="preview"] > a')
+        if items:
+            url = self.http().normalize_uri(items[0].get('href'))
+            content = self.http_get(url)
+            images = self.re.search(r'chapters\s*=\s*(\{.+\})\s*;', content)
+            if images:
+                return self._images(images.group(1))
         return []
 
     def get_cover(self) -> str:
-        # return self._cover_from_content('.cover img')
-        pass
+        return self._cover_from_content('.cover > a > img')
 
 
 main = _Template
