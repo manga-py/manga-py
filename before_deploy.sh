d4f749997e63107b5b00399dcbf6fc6d2bdc2cf7
1271 <sttv-pc@mail.ru>
2018-03-03 19:24:38 +0300

    patch deploy condition (test)

--- before_deploy.sh
+++ before_deploy.sh
@@ -10,7 +10,21 @@ if [ c -ge 1 ]
     if [[ "${py_version:7:-2}" = "3.6" ]]
       then
         allow_deploy="true"
+        echo "Start make gh-pages content"
+        cp helpers/.builder.py .
+        cp helpers/.providers_updater.py .
+        cp helpers/manga.spec .
+        python .providers_updater.py
+        lessc helpers/gh_pages_content/style.less helpers/gh_pages_content/style.css
+        html-minifier helpers/gh_pages_content/index.html --output helpers/gh_pages_content/index.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
+        html-minifier helpers/gh_pages_content/improvement.html --output helpers/gh_pages_content/improvement.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
+        git add -A
+        git commit -a -m upd
+        if [[ "$TRAVIS_TAG" != "" ]]; then
+            echo "Start build bin package"
+            pyinstaller manga.spec --log-level CRITICAL -y -F
+        fi
       else
-        echo "${py_version:7:-2}"
+        echo "Skip for ${py_version:7:-2}"
     fi
 fi
