7e6c5190615438ae2a974b44bacebfc226b9f9ef
1271 <sttv-pc@mail.ru>
2018-03-03 20:10:09 +0300

    Change .travi.yml

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,8 +4,11 @@ allow_deploy="false"
 
 py_version=$(python --version)
 
-if [[ "$ALLOW_DEPLOY" = "true" ]]
+if [[ "${py_version:7:-2}" = "3.6" ]]
   then
+    echo "Deploy! ($TRAVIS_TEST_RESULT)"
+    ALLOW_DEPLOY="true"
+    ./cc-test-reporter after-build -t coverage.py --exit-code $TRAVIS_TEST_RESULT || true
     if [[ "$TRAVIS_TAG" != "" ]]; then
         echo "Start build bin package"
         pyinstaller manga.spec --log-level CRITICAL -y -F
