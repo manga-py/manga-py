6989734c04fc27af35bdf94ed374e20b2a582993
1271 <sttv-pc@mail.ru>
2018-03-03 19:53:24 +0300

    Change .travi.yml

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,23 +4,10 @@ allow_deploy="false"
 
 py_version=$(python --version)
 
-if [[ "${py_version:7:-2}" = "3.6" ]]
+if [[ "$ALLOW_DEPLOY" = "true" ]]
   then
-    allow_deploy="true"
-    echo "Start make gh-pages content"
-    cp helpers/.builder.py .
-    cp helpers/.providers_updater.py .
-    cp helpers/manga.spec .
-    python .providers_updater.py
-    lessc helpers/gh_pages_content/style.less helpers/gh_pages_content/style.css
-    html-minifier helpers/gh_pages_content/index.html --output helpers/gh_pages_content/index.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
-    html-minifier helpers/gh_pages_content/improvement.html --output helpers/gh_pages_content/improvement.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
-    git add -A
-    git commit -a -m upd
     if [[ "$TRAVIS_TAG" != "" ]]; then
         echo "Start build bin package"
         pyinstaller manga.spec --log-level CRITICAL -y -F
     fi
-  else
-    echo "Skip for ${py_version:7:-2}"
 fi
