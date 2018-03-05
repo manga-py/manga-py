a5b609810c925e749701b60d7d06b36f2437367f
1271 <sttv-pc@mail.ru>
2018-03-05 11:58:53 +0300

    Test patch .travis.yml

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,16 +4,6 @@ if [[ "$allow_deploy" = "true" ]]
 then
 echo "Deploy! ($TRAVIS_TEST_RESULT)"
 
-cp helpers/.builder.py .
-cp helpers/.providers_updater.py .
-cp helpers/manga.spec .
-python .providers_updater.py
-lessc helpers/gh_pages_content/style.less helpers/gh_pages_content/style.css
-html-minifier helpers/gh_pages_content/index.html --output helpers/gh_pages_content/index.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
-html-minifier helpers/gh_pages_content/improvement.html --output helpers/gh_pages_content/improvement.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
-git add -A
-git commit -a -m upd
-
 coverage xml
 ./cc-test-reporter after-build -t coverage.py --exit-code $TRAVIS_TEST_RESULT || true
 
