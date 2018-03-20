289f1080be01dbb8c392ec168f22f607a0aa9d88
1271 <sttv-pc@mail.ru>
2018-03-20 18:39:25 +0300

    Update gh-pages

---
+++ helpers/gh_pages_content/download_btns.js
@@ -0,0 +1,44 @@
+
+((d) => {
+    d.addEventListener('DOMContentLoaded', () => {
+        if(typeof repoUrl == 'undefined')
+        {
+            return;
+        }
+        fetch(repoUrl)
+            .then(response => response.json())
+            .then((response) => {
+                const links = d.querySelector('#download-links');
+                const tar = links.querySelector('.tar');
+                const zip = links.querySelector('.zip');
+
+                tar.setAttribute('href', response.tarball_url);
+                tar.setAttribute('active', 'true');
+                zip.setAttribute('href', response.zipball_url);
+                zip.setAttribute('active', 'true');
+            });
+        const ul = d.querySelector('ul.list');
+        if(!ul)
+        {
+            return;
+        }
+        fetch('./providers.json')
+            .then(response => response.json())
+            .then((list) => {
+                let html = '', m = 0, done = 0;
+                for(let i in list) {
+                    if (!list.hasOwnProperty(i)) continue;
+                    m+=1;
+                    html += '<li><input id="I' + m + '" type="checkbox" ' +
+                        (list[i][1] ? 'checked="" ' : '') +
+                        'disabled=""><label for="I' + m + '"></label><span>' +
+                        '<a target="_blank" href="' +
+                        list[i][0] + '">' +
+                        list[i][0] + '</a> ' +
+                        list[i][2] + '</span></li>';
+                    done += parseInt(list[i][1]);
+                }
+                ul.innerHTML = html + ('<!-- ' + list.length + ' ( ' + done + ' ) -->');
+            });
+    });
+})(document);
