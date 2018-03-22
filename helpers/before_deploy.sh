43a7b7228070b3bf0add0a231ef7da0f46c3ed33
1271 <sttv-pc@mail.ru>
2018-03-22 14:20:40 +0300

    update .travis.yml

--- helpers/before_deploy.sh
+++ helpers/before_deploy.sh
@@ -5,12 +5,20 @@ then
 
     if [[ "$TRAVIS_TAG" != "" ]]
     then
-        echo "Start build bin package"
-        cp helpers/.builder.py .
-        cp helpers/.providers_updater.py .
-        cp helpers/manga.spec .
-        python .providers_updater.py
-        pyinstaller manga.spec --log-level CRITICAL -y -F
+        echo "Skip build bin package"
+#        echo "Start build bin package"
+#        cp helpers/.builder.py .
+#        cp helpers/.providers_updater.py .
+#        cp helpers/manga.spec .
+#        python .providers_updater.py
+#        pyinstaller manga.spec --log-level CRITICAL -y -F
+    else
+        echo "Make gh-pages"
+        lessc helpers/gh_pages_content/style.less helpers/gh_pages_content/style.css
+        html-minifier helpers/gh_pages_content/index.html --output helpers/gh_pages_content/index.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
+        html-minifier helpers/gh_pages_content/improvement.html --output helpers/gh_pages_content/improvement.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
+        git add -A
+        git commit -a -m upd
     fi
 
 fi
