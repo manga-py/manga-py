51303b2112010c631ac9c4753835b7ce39cb6a9e
1271 <sttv-pc@mail.ru>
2018-03-22 17:16:23 +0300

    update .travis.yml

--- helpers/before_script.sh
+++ helpers/before_script.sh
@@ -7,7 +7,7 @@ py_version=$(python --version)
 if [[ "${py_version:7:-2}" = "3.5" ]]
 then
 allow_deploy="true"
+fi
 if [[ "$TRAVIS_TAG" = "" ]]
 then wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -O /tmp/chrome.deb && sudo dpkg -i /tmp/chrome.deb && sudo apt-get install -y -f --fix-missing
-fi
 fi
