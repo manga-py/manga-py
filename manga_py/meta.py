b928748793a82ba2853835aef6837e03904f57fe
1271 <sttv-pc@mail.ru>
2018-08-01 21:45:43 +0300

    Patch #93

--- manga_py/meta.py
+++ manga_py/meta.py
@@ -1,3 +1,3 @@
-__version__ = '1.3.17'
+__version__ = '1.3.18'
 __repo_name__ = 'yuru-yuri/manga-dl'
 __downloader_uri__ = 'https://github.com/%s' % __repo_name__
--- manga_py/providers/trashscanlations_com.py
+++ manga_py/providers/trashscanlations_com.py
@@ -2,6 +2,12 @@ from .zeroscans_com import ZeroScansCom
 
 
 class TrashScanlationsCom(ZeroScansCom):
+    def get_chapter_index(self) -> str:
+        ch = self.chapter
+        idx = self.re.search(self._chapter_selector, ch)
+        idx = self.re.split(r'[^\d]', idx.group(1))
+        return '-'.join(idx)
+
     def get_main_content(self):
         return self._get_content('{}/series/{}/')
 
