3df240301684cf855e6b582defa58dd347f6f6f9
1271 <sttv-pc@mail.ru>
2019-04-16 15:59:34 +0300

    Patch #163

--- manga_py/providers/webtoons_com.py
+++ manga_py/providers/webtoons_com.py
@@ -5,6 +5,7 @@ from .helpers.std import Std
 class WebToonsCom(Provider, Std):
     __titleNo = 0
     __mainUrl = ''
+    __next_page_urls = None
 
     def get_archive_name(self) -> str:
         i = self.re.search(r'\.com%s%s' % (
@@ -28,14 +29,38 @@ class WebToonsCom(Provider, Std):
     def _chapters(self, content):
         return self._elements('#_listUl li > a', content)
 
+    def _get_page_urls(self, content):
+        return self._elements('.paginate a:not([class]):not([onclick])', content)
+
+    def _get_pages_urls(self, content):
+        chapters = []
+        n = self.http().normalize_uri
+        for j in self._get_page_urls(content):  # page-urls
+            _content = self.http_get(n(j.get('href')))
+            chapters += self._chapters(_content)
+        return chapters
+
+    def get_next_page_urls(self, content):
+        urls = self._elements('a + a.pg_next', content)
+        n = self.http().normalize_uri
+        if len(urls):
+            self.__next_page_urls.append(n(urls[0].get('href')))
+            _content = self.http().get(n(urls[0].get('href')))
+            self.get_next_page_urls(_content)
+
     def get_chapters(self):
-        pages = self._elements('.paginate a + a')
+        print('Parse chapters. Please, wait')
+        self.__next_page_urls = []
         chapters = self._chapters(self.content)
-        if pages:
-            n = self.http().normalize_uri
-            for i in pages:
-                content = self.http_get(n(i.get('href')))
-                chapters += self._chapters(content)
+        n = self.http().normalize_uri
+        chapters += self._get_pages_urls(self.content)  # main page paginator
+
+        self.get_next_page_urls(self.content)
+        for url in self.__next_page_urls:
+            content = self.http().get(n(url))
+            chapters += self._chapters(content)
+            chapters += self._get_pages_urls(content)
+
         return chapters
 
     def get_files(self):
