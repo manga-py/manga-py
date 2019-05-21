a83b5f8892bdfb9dad79ac869db12ab2e776e49c
1271 <sttv-pc@mail.ru>
2019-05-21 16:03:58 +0300

    Update log config example

---
+++ manga_py/libs/db/__init__.py
@@ -0,0 +1,60 @@
+from binascii import crc32
+from datetime import datetime
+from pathlib import Path
+
+import peewee
+
+from manga_py.libs.fs import system_path
+
+__cache = {}
+
+
+def db_path() -> Path:
+    """
+    Overload this to change the path to the database
+    :return:
+    """
+    return system_path().joinpath('manga.db')
+
+
+def make_db(force=False):
+    path = db_path()
+    force and path.unlink()
+    if not path.is_file():
+        _db_cache().create_tables([Manga], safe=True)
+
+
+def _db_cache() -> peewee.Database:
+    path = db_path()
+    crc = crc32(str(path).encode())
+    if __cache.get(crc, None) is None:
+        __cache[crc] = peewee.SqliteDatabase(path)
+    return __cache[crc]
+
+
+class Manga(peewee.Model):
+    key = peewee.CharField(unique=True)
+    url = peewee.CharField()
+    name = peewee.CharField()
+    path = peewee.CharField(max_length=2047)
+    active = peewee.BooleanField(default=True)
+    created_at = peewee.DateTimeField(default=datetime.now())
+    updated_at = peewee.DateTimeField(null=True)
+    chapters = peewee.TextField()  # downloaded chapters list. JSON data
+    data = peewee.TextField()  # cookies, browser, args. JSON data
+
+    @classmethod
+    def update_or_insert(cls, data: dict, key='key'):
+        db = cls.get_or_none(getattr(Manga, key) == data.get(key))
+        del data['created_at']
+        del data[key]
+        if db is not None:
+            db.update(**data, updated=datetime.now())
+            db.save()
+        else:
+            del data['updated_at']
+            db.create(**data)
+
+    class Meta:
+        database = _db_cache()
+        table_name = 'manga'
--- manga_py/libs/log.py
+++ manga_py/libs/log.py
@@ -5,16 +5,25 @@ import yaml
 
 from manga_py.libs import fs
 
-config_file = fs.system_path().joinpath('.logger.config.yaml')
+__cache = {}
 
-# if in-home-directory config not exists, copy this
-if not config_file.is_file():
-    src = fs.root_path().joinpath('.logger.config.yaml')
-    shutil.copy(str(src), str(config_file))
 
-with open('.yaml', 'r') as f:
-    config = yaml.safe_load(f.read())
-    logging.config.dictConfig(config)
+def _get():
+    config_file = fs.system_path().joinpath('.logger.config.yaml')
 
-logger = logging.getLogger(__name__)
+    # if in-home-directory config not exists, copy this
+    if not config_file.is_file():
+        src = fs.root_path().joinpath('.logger.config.yaml')
+        shutil.copy(str(src), str(config_file))
 
+    with open('.yaml', 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+
+    return logging.getLogger(__name__)
+
+
+def logger():
+    if __cache.get('logger', None) is None:
+        __cache['logger'] = _get()
+    return __cache['logger']
