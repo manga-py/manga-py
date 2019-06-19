34e65b0d5044ef1fc00817481c192bf76473e615
Sergey <sttv-pc@mail.ru>
2019-06-19 20:46:00 +0300

    Patch ManhwaHentaiCom provider #188

--- manga_py/providers/manhwahentai_com.py
+++ manga_py/providers/manhwahentai_com.py
@@ -5,7 +5,11 @@ from .helpers.std import Std
 class ManhwaHentaiCom(Provider, Std):
 
     def get_chapter_index(self) -> str:
-        return self.re.search(r'/(?:chapter|epiode)-(\d+(?:-\d+)?)', self.chapter).group(1)
+        try:
+            return self.re.search(r'/manhwa/[^/]+/[\w-]+-(\d+(?:-\d+)?)', self.chapter).group(1)
+        except AttributeError:
+            print(self.chapter)
+            exit(1)
 
     def get_main_content(self):
         return self._get_content('{}/manhwa/{}')
