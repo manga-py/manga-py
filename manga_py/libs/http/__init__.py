ee8b57b9f531838779288c993e359ea14ade7b79
Sergey <sttv-pc@mail.ru>
2019-06-19 20:35:35 +0300

    Update libs.http

--- manga_py/libs/http/__init__.py
+++ manga_py/libs/http/__init__.py
@@ -1,13 +1,15 @@
-from requests import Response, PreparedRequest
+from pathlib import Path
+from random import randint
+from urllib.parse import ParseResult, urlparse
+from zlib import crc32
+
+from requests import Response
 from requests.api import get, post, request
 
+from manga_py import exceptions
+from manga_py.libs import print_lib
 from manga_py.libs.log import logger
 from manga_py.libs.store import http_store, Store
-from manga_py import exceptions
-from urllib.parse import ParseResult, urlparse
-from pathlib import Path
-from zlib import crc32
-from random import randint
 
 
 class Http:
@@ -21,42 +23,58 @@ class Http:
         return self.store.arguments.show_log
 
     def download(self, response: Response, destination: Path):
-        if self.__debug():
-            r = response.request  # type: PreparedRequest
-            logger().debug('\n'.join([
-                '\nDownload file',
-                'Method: ' + r.method,
-                'Url: ' + r.url,
-                ''
-            ]))
+        self.__debug_url(response.request.method, response.request.url)
         with destination.open('wb') as w:
             if not w.writable():
                 raise exceptions.FsError('Destination not writable. Please, check permissions')
             content = response.content
-            if not isinstance(content, bytes):
+            errors = []
+            if ~response.headers['Content-Type'].find('text/'):
+                errors.append('Content not binary type.')
+            if len(content) <= 0:
+                errors.append('Response has zero length.')
+                print_lib(
+                    '%s Url: %s\nHistory: %s' %
+                    (
+                        errors[-1],
+                        response.url,
+                        '\n'.join(map(lambda _url: _url.url, response.history))
+                    )
+                )
+            if len(errors) > 0:
                 error_file = str(crc32(str(destination).encode()))
-                with Path(error_file).open('w') as _w:
+                with Path(error_file).open('wb') as _w:
                     _w.write(content)
-                raise exceptions.NetworkException('Content not bytes. Check ' + error_file)
-            w.write(content)
+                logger().warning('\n'.join(errors))
+                w.close()
+                destination.unlink()  # remove, if error
+            else:
+                w.write(content)
+                w.close()
+                return True
 
     def request(self, method, url, **kwargs):
         if self.base_url is None:
             self.base_url = url
         __doc__ = request.__doc__
-        if self.__debug():
-            logger().debug('\n'.join([
-                'Method: ' + method,
-                'Url: ' + url,
-            ]))
+        self.__debug_url(method, url)
         try:
             return request(method, url, **kwargs)
         finally:
             raise exceptions.NetworkException()
 
+    def __debug_url(self, method, url):
+        if self.__debug():
+            logger().debug('\n'.join([
+                'Method: ' + method,
+                'Url: ' + url,
+                ''
+            ]))
+
+
     def url_normalize(self, url):
         _url = urlparse(url)  # type: ParseResult
-        if not _url.scheme:
+        if _url.scheme == '':
             _url.scheme = self.store.scheme
         return _url
 
@@ -70,7 +88,8 @@ class Http:
         __doc__ = post.__doc__
         return post(url, data, json, **kwargs)
 
-    def _domain_from_url(self, url):
+    @staticmethod
+    def _domain_from_url(url):
         fragments = urlparse(url)  # type: ParseResult
         if len(fragments.netloc) < 1:
             raise exceptions.InvalidUrlException(url)
@@ -96,6 +115,21 @@ def default_ua():
         # 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
     ]
     return agents[randint(0, len(agents) - 1)]
+
+
+def check_url(url: str) -> str:
+    """
+    checks url for correctness
+    :param url:
+    :return:
+    """
+    check = urlparse(url)  # type: ParseResult
+    if check.scheme == '':
+        logger().warning('Url scheme has missing (%s). Use default scheme' % url)
+    if check.netloc == '':
+        logger().critical('Url netloc has missing (%s)')
+        raise exceptions.InvalidUrlException(url)
+    return check.geturl()
 
 
 __all__ = ['Http', 'default_ua']
