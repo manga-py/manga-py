f4bb7d739c7b097c5674887e62dad47eb572372b
Sergey <sttv-pc@mail.ru>
2020-01-11 16:25:09 +0300

    Update .travis.yml

--- helpers/before_deploy.sh
+++ helpers/before_deploy.sh
@@ -1,24 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ "$allow_deploy" = "true" ]]
+if [[ "$TRAVIS_TAG" = "" ]]
 then
-
-    if [[ "$TRAVIS_TAG" != "" ]]
-    then
-        echo "Skip build bin package"
-#        echo "Start build bin package"
-#        cp helpers/.builder.py .
-#        cp helpers/.providers_updater.py .
-#        cp helpers/manga.spec .
-#        python .providers_updater.py
-#        pyinstaller manga.spec --log-level CRITICAL -y -F
-    else
-        echo "Make gh-pages"
-        node-sass helpers/gh_pages_content/style.scss helpers/gh_pages_content/style.css --output-style compressed
-        html-minifier helpers/gh_pages_content/index.html --output helpers/gh_pages_content/index.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
-        html-minifier helpers/gh_pages_content/improvement.html --output helpers/gh_pages_content/improvement.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
-        git add -A
-        git commit -a -m upd
-    fi
-
+    echo "Make gh-pages"
+    node-sass helpers/gh_pages_content/style.scss helpers/gh_pages_content/style.css --output-style compressed
+    html-minifier helpers/gh_pages_content/index.html --output helpers/gh_pages_content/index.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
+    html-minifier helpers/gh_pages_content/improvement.html --output helpers/gh_pages_content/improvement.html --html5 --remove-comments --remove-tag-whitespace --collapse-inline-tag-whitespace --remove-attribute-quotes --collapse-whitespace
+    git add -A
+    git commit -a -m upd
 fi
--- helpers/before_script.sh
+++ helpers/before_script.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-allow_deploy="false"
-
 py_version=$(python --version)
 
-if [[ "${py_version:7:-2}" = "3.5" ]]
-then
-allow_deploy="true"
-fi
 if [[ "$TRAVIS_TAG" = "" ]]
 then wget 'https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb' -O /tmp/chrome.deb && sudo dpkg -i /tmp/chrome.deb && sudo apt-get install -y -f --fix-missing
 fi
