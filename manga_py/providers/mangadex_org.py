4ae61ad3633c81adbddd49e19028b5a88078cd6b
Sergey <sttv-pc@mail.ru>
2020-01-20 09:10:21 +0300

    Update mangadex #257

--- manga_py/providers/mangadex_org.py
+++ manga_py/providers/mangadex_org.py
@@ -5,15 +5,59 @@ from .helpers.std import Std
 class MangaDexOrg(Provider, Std):
     __content = None
     __chapters = None
+    __languages = None
+    __countries = {
+        '': 'Other',
+        'bd': 'Bengali',
+        'bg': 'Bulgarian',
+        'br': 'Portuguese (Br)',
+        'cn': 'Chinese (Simp)',
+        'ct': 'Catalan',
+        'cz': 'Czech',
+        'de': 'German',
+        'dk': 'Danish',
+        'es': 'Spanish (Es)',
+        'fi': 'Finnish',
+        'fr': 'French',
+        'gb': 'English',
+        'gr': 'Greek',
+        'hk': 'Chinese (Trad)',
+        'hu': 'Hungarian',
+        'id': 'Indonesian',
+        'il': 'Hebrew',
+        'in': 'Hindi',
+        'ir': 'Persian',
+        'it': 'Italian',
+        'jp': 'Japanese',
+        'kr': 'Korean',
+        'lt': 'Lithuanian',
+        'mm': 'Burmese',
+        'mn': 'Mongolian',
+        'mx': 'Spanish (LATAM)',
+        'my': 'Malay',
+        'nl': 'Dutch',
+        'no': 'Norwegian',
+        'ph': 'Filipino',
+        'pl': 'Polish',
+        'pt': 'Portuguese (Pt)',
+        'ro': 'Romanian',
+        'rs': 'Serbo-Croatian',
+        'ru': 'Russian',
+        'sa': 'Arabic',
+        'se': 'Swedish',
+        'th': 'Thai',
+        'tr': 'Turkish',
+        'ua': 'Ukrainian',
+        'vn': 'Vietnamese',
+    }
 
     def get_archive_name(self) -> str:
-        return self.normal_arc_name({
-            'vol': self.chapter['volume'],
-            'ch': self.chapter['chapter'],
-        })
+        prev = super().get_archive_name()
+        code = self.chapter['lang_code']
+        return '{}-{}'.format(prev, self.__countries[code])
 
     def get_chapter_index(self) -> str:
-        return self.chapter_for_json()
+        return self.chapter['chapter'].replace('.', '-')
 
     def manga_idx(self):
         return self.re.search(r'/(?:manga|title)/(\d+)', self.get_url()).group(1)
@@ -43,20 +87,25 @@ class MangaDexOrg(Provider, Std):
             languages = self.quest(
                 [],
                 'Available languages:\n{}\n\nPlease, select your lang (empty for all, space for delimiter lang):'.format(
-                    '\n'.join(self.languages())
+                    '\n'.join(self._languages)
                 )).split(' ')
             self.__chapters = self.filter_chapters(languages)
         return self.__chapters
 
-    def languages(self) -> list:
-        languages = []
+    @property
+    def _languages(self) -> list:
+        if self.__languages is None:
+            self.__languages = []
+            self.__fill_languages()
+        return self.__languages
+
+    def __fill_languages(self):
         for lang in self._chapters:
-            if lang['lang_code'] not in languages:
-                languages.append(lang['lang_code'])
-        return languages
+            if lang['lang_code'] not in self.__languages:
+                self.__languages.append(lang['lang_code'])
 
     def filter_chapters(self, languages: list) -> list:
-        if len(languages) == 0 or languages[0] == '':
+        if len(languages) == 0 or languages[0].strip(' ') == '':
             return self._chapters
         return [chapter for chapter in self._chapters if chapter['lang_code'] in languages]
 
@@ -74,7 +123,7 @@ class MangaDexOrg(Provider, Std):
         )
 
     def chapter_for_json(self) -> str:
-        return '{}-{}'.format(self.chapter['volume'], self.chapter['chapter'])
+        return '{}-{}'.format(self.chapter['volume'] or '0', self.chapter['chapter'])
 
 
 main = MangaDexOrg
