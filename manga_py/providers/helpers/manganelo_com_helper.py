c605afd421f8a35ac675c39067d7f336253325f4
gromenauer <39655925+gromenauer@users.noreply.github.com>
2020-05-09 14:41:50 +0200

    🐛 Fix manganelo check_alternative_server regexp

--- manga_py/providers/helpers/manganelo_com_helper.py
+++ manga_py/providers/helpers/manganelo_com_helper.py
@@ -9,6 +9,6 @@ def check_alternative_server(images: list, other_cdn: str):
             with get(images[0], stream=True) as image:
                 image.raise_for_status()
         except HTTPError:
-            re = compile('^.*//[^/](/.+)$')
+            re = compile('^.*//[^/]*(/.+)$')
             return ['%s%s' % (other_cdn, re.search(i).group(1)) for i in images]
     return images
