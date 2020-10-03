ae1df5429a7dae8a370d8ac05c4fb30f07d701d3
1271 <sttvpc@gmail.com>
2020-10-03 12:23:47 +0300

    Comments for providers._template

--- manga_py/providers/_template.py
+++ manga_py/providers/_template.py
@@ -3,32 +3,48 @@ from .helpers.std import Std
 
 
 class _Template(Provider, Std):
-
     def get_archive_name(self) -> str:
-        pass
+        """
+        Allows you to overload name generation. Method may be missing
+        """
 
     def get_chapter_index(self) -> str:
-        pass
+        """
+        Any string separated by "-"
+        Example: "1-3"
+        """
 
     def get_main_content(self):
-        pass
+        """
+        Called second. Returns the initial content to parse (just a cache)
+        Can be obtained using self.content
+        """
 
     def get_manga_name(self) -> str:
-        return ''
+        """
+        String for the name of the manga directory
+        """
 
     def get_chapters(self):
+        """
+        Should return an array of data (you can return the result of the self._elements ('css selector') method)
+        """
         # return self._elements('a.chapter')
         return []
 
     def get_files(self):
+        """
+        Should return an array of strings (url of images)
+        """
         return []
 
     def get_cover(self) -> str:
+        """Not used now"""
         # return self._cover_from_content('.cover img')
-        pass
 
     def book_meta(self) -> dict:
         """
+        Not used now
         :see http://acbf.wikia.com/wiki/Meta-data_Section_Definition
         return {
             'author': str,
@@ -39,10 +55,13 @@ class _Template(Provider, Std):
             'rating': str,
           }
         """
-        pass
 
     def chapter_for_json(self) -> str:
-        # overload std param, if need
+        """
+        overload std param, if need
+        If present, overloads getting chapter name for json dump
+        Should return a string
+        """
         # return self.chapter
         pass
 
