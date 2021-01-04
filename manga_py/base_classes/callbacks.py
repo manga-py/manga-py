d94f3ab2b7588cf03eded897a1b2eeaa9aec5908
GAUGRY Thierry <thierry.gaugry@inria.fr>
2021-01-05 00:19:21 +0100

    Add global progress/chapter progress

--- manga_py/base_classes/callbacks.py
+++ manga_py/base_classes/callbacks.py
@@ -3,15 +3,18 @@ from typing import Callable
 
 class Callbacks:
     def _call_files_progress_callback(self):
-        if callable(self.progress):
+        if callable(self.chapter_progress):
             _max, _current = len(self._storage['files']), self._storage['current_file']
-            self.progress(_max, _current, _current < 1)
+            self.chapter_progress(_max, _current, _current < 1)
 
     def set_quest_callback(self, callback: Callable):  # Required call from initiator (CLI, GUI)
         setattr(self, 'quest', callback)
 
-    def set_progress_callback(self, callback: Callable):  # Required call from initiator (CLI, GUI)
-        setattr(self, 'progress', callback)
+    def set_chapter_progress_callback(self, callback: Callable):  # Required call from initiator (CLI, GUI)
+        setattr(self, 'chapter_progress', callback)
+
+    def set_global_progress_callback(self, callback: Callable):  # Required call from initiator (CLI, GUI)
+        setattr(self, 'global_progress', callback)
 
     def set_log_callback(self, callback: Callable):  # Required call from initiator (CLI, GUI)
         setattr(self, 'log', callback)
